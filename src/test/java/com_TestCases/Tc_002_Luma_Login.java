51b9b11dc2dd30ad21a1449963445f91420f2831
Anwesh25 <anweshsriramoju7475@gmail.com>
2025-04-16 12:51:33 +0530

    Tl commit

--- src/test/java/com_TestCases/Tc_002_Luma_Login.java
+++ src/test/java/com_TestCases/Tc_002_Luma_Login.java
@@ -5,16 +5,18 @@ import org.testng.annotations.Test;
 import com_Luma_page_object.Login_Page;
 
 public class Tc_002_Luma_Login extends Luma_BaseClass {
-	/*
+	
 	public Login_Page lp;
 	@Test
 	public void Luma_Login() {
 		
 		lp= new Login_Page(driver);
-		lp.EnterEmail(LoginEmail);
-		lp.EnterPassword(LoginPassword);
+	//	lp.EnterEmail(email);
+		lp.EnterEmail(EmailAdd);
+	//	lp.EnterPassword(password);
+		lp.EnterPassword(CPassword);
 		lp.ClickOnSignIn();
 		
 	}
-*/
+
 }
